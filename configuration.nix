e5bc360426b9e38679a73717db35f12c4b167dbe
dprive <raphaelchouraqui92@gmail.com>
2026-04-25 08:17:41 +0200

    Install edge and Unfree software = true

--- configuration.nix
+++ configuration.nix
@@ -1,123 +1,161 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  boot.loader = {
+	  systemd-boot.enable = true;
+	  efi.canTouchEfiVariables = true;
+  };
+
+  networking = {
+		  hostName = "raph-framework";
+		  networkmanager = {
+				  enable = true;
+				  wifi = {
+						  powersave = false;
+						  macAddress = "preserve";
+				  };
+		  };
+  };
+
+  time.timeZone = "Europe/Paris";
+
+
+  documentation = {
+    enable = true;
+    man.enable = true;
+    dev.enable = true;
+  };
+
+  nix = {
+    gc = {
+      automatic = true;
+      dates = "daily";
+      options = "--delete-older-than 14d";
+    };
+    settings = {
+      download-buffer-size = 268435456;
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      max-jobs = "auto";
+      auto-optimise-store = true;
+    };
+  };
+
+  security = {
+    polkit.enable = true;
+    pam.services = {
+      greetd = {
+        enableGnomeKeyring = true;
+        fprintAuth = true;
+      };
+      login.fprintAuth = true;
+      sudo.fprintAuth = true;
+      hyprlock.text = ''
+        auth sufficient pam_fprintd.so
+        auth include login
+      '';
+    };
+  };
+
+  services = {
+	gnome.gnome-keyring.enable = true;
+	seatd.enable = true;
+	blueman.enable = true;
+	fprintd = {
+	  enable = true;
+	  package = pkgs.fprintd-tod;
+	  tod = {
+		enable = true;
+		driver = pkgs.libfprint-2-tod1-goodix;
+	  };
+	};
+	greetd = {
+	  enable = true;
+	  settings = {
+		default_session = {
+		  command = "${pkgs.tuigreet}/bin/tuigreet --remember --user-menu --remember-user-session --time";
+		};
+	  };
+	  useTextGreeter = true;
+	};
+	dbus.enable = true;
+	pipewire = {
+	  enable = true;
+	  alsa.enable = true;
+	  alsa.support32Bit = true;
+	  pulse.enable = true;
+	  jack.enable = true;
+	};
+	udev = {
+	  packages = with pkgs; [
+		libfprint-2-tod1-goodix
+	  ];
+	  extraRules = ''
+		SUBSYSTEM=="usb", ATTR{idVendor}=="0483", ATTR{idProduct}=="5740", MODE="0666"
+		'';
+	};
+  };
+
+  virtualisation.docker.enable = true;
+
+  xdg.portal = {
+	enable = true;
+	extraPortals = [
+	  pkgs.xdg-desktop-portal-hyprland
+		pkgs.xdg-desktop-portal-gtk
+	];
+	config.common.default = "*";
+  };
+
+
+
+  users.users.raph = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    packages = with pkgs; [
+		eza
+		kitty
+		home-manager
     ];
-
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  # networking.hostName = "nixos"; # Define your hostname.
-
-  # Configure network connections interactively with nmcli or nmtui.
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  };
 
 
+  hardware = {
+    enableRedistributableFirmware = true;
+    bluetooth.enable = true;
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+  };
+
+  programs = {
+	firefox.enable = true;
+	thunar.enable = true;
+	hyprland = {
+	  enable = true;
+	  xwayland.enable = true;
+	};
+  };
   
+  nixpkgs.config.allowUnfree = true;
 
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+    microsoft-edge
+  ];
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # services.pulseaudio.enable = true;
-  # OR
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  # };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.alice = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  #   packages = with pkgs; [
-  #     tree
-  #   ];
-  # };
-
-  # programs.firefox.enable = true;
-
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
-  # environment.systemPackages = with pkgs; [
-  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "25.11"; # Did you read the comment?
-
+  system.stateVersion = "25.11";
 }
-
