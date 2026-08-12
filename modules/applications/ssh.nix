1788efb39261df7720bcbdafc7dcb44070e9a298
dprive <raphaelchouraqui92@gmail.com>
2026-08-12 17:23:12 +0200

    build(aplications[ssh]: create ssh.nix

---
+++ modules/applications/ssh.nix
@@ -0,0 +1,41 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
+let
+  cfg = config.applications.ssh;
+  sshKeyFramework = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHRkJ2N9gqghjbtg6gOrfu4g7hRsJylDeCmKg/pqJ1vx raph@raph-framework";
+  sshKeyMac = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG2wcAfGuJ9zXIhQANVrM7s48pbQQWwaUqPwqUuds2Lr raph@MacBookPro.lan";
+  #ajouter la clé ssh des servers 1, 2, 3
+in
+
+{
+  config = lib.mkIf cfg {
+    users.users.raph.openssh.authorizedKeys.keys = [
+      sshKeyMac
+      sshKeyFramework
+    ];
+
+    services.openssh = {
+      enable = true;
+      ports = [ 23305 ];
+      openFirewall = true;
+      settings = {
+        PasswordAuthentication = false;
+        KbdInteractiveAuthentication = false;
+        PermitRootLogin = "no";
+        AllowUsers = [ "raph" ];
+        MaxAuthTries = 3;
+        MaxSessions = 5;
+        ClientAliveInterval = 300;
+        ClientAliveCountMax = 2;
+      };
+    };
+    networking.firewall.allowedTCPPorts = [
+      23305
+    ];
+  };
+}
